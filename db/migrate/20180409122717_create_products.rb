9ab49277206e91c289ef293d746fee3c6803b58e
daniel-modenesi <modenesi.daniel@gmail.com>
2018-04-09 09:43:54 -0300

    first migrate

--- db/migrate/20180409122717_create_products.rb
+++ db/migrate/20180409122717_create_products.rb
@@ -1,10 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
-      t.string : title
+      t.string :title
       t.text :description
       t.string :iamge_url
-      t.decimal :price
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
